3359e668654cc3d958a7fda3b0e2ef9851ff5d93
mastablasta1 <tomasz.idziak@gmail.com>
2014-04-23 13:31:55 +0200

    Dodanie szkieletu

---
+++ ICanTransferMoney/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICanTransferMoney
+{
+    interface ICanTransferMoney
+    {
+        public bool transferMoney(Account from, Account to);
+    }
+}

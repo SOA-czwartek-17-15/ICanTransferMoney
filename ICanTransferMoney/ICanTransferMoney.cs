885f0e1a0241180b9b0833c27a3a967a92d2e866
the-Mitu <the.mail.of.mi2@gmail.com>
2014-04-24 16:39:10 +0200

    Zajęcia z 24 kwietnia

---
+++ ICanTransferMoney/ICanTransferMoney.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ICanTransferMoney
+{
+    [ServiceContract]
+    public interface ICanTransferMoney
+    {
+        [OperationContract]
+        bool transferMoney(Account from, Account to);
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class MoneyTransferer : ICanTransferMoney
+    {
+        public bool transferMoney(Account from, Account to)
+        {
+            Console.Out.WriteLine("Not implemented yet");
+            return false;
+        }
+    }
+}
---
+++ ICanTransferMoney/IServiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace ICanTransferMoney
+{
+    [ServiceContract]
+    interface IServiceRepository
+    {
+        [OperationContract]
+        void RegisterService(string name, string address);
+    }
+}
